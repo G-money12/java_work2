f293ff213909d120f9f0091014ebb54102a2ce48
DESKTOP-GNR93RH\it <jjjw19830707@gmail.com>
2025-01-06 16:14:32 +0900

    첫번째 커밋

---
+++ Project1/src/project1/Test01.java
@@ -0,0 +1,12 @@
+package project1;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, world");
+	}
+
+}
